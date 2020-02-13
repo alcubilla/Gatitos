b430cdc3c20a9d4e88292bc70ed2e2355ffb15d8
Marina <alcubilla.marina@gmail.com>
2020-02-13 17:46:01 -0600

    Se modifico para poner dentro de Directory el renderizado de la información

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
-import React, {Component} from "react";
-import ContactCard from "./components/ContactCard";
-import contactsData from "./contactsData";
+import React from 'react';
+import Directory from "./components/Directory"
 
-class App extends Component {
-constructor(){
-super();
-  this.state = {
-    elemento : contactsData
-}
-
-  }
-
- 
-
-
-render () {
-  const cards = this.state.elemento.map((contact, idx) => 
-  <ContactCard info={contact} key={idx} />);
-//IF ELSE
-let  contenido;
-if (cards.length > 0)
- {
-  contenido = cards;
- } else {
-   contenido = <p>No hay contactos disponibles</p>;
- }
-return(
-  <>
-  {contenido}
-  </>
-);
-
-}
-
-}
-
+const App = () => {
+  return (
+    <Directory />
+  );
+};
 
 export default App;
